ca3591cd2ea89b3f8f6ed6e58222ed48ea110b7d
Mx-Luis <luismt_4000@outlook.com>
2025-05-17 12:29:14 -0500

    Add files via upload

---
+++ src/app/acerca-de/acerca-de.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AcercaDeRoutingModule } from './acerca-de-routing.module';
+import { AcercaDeComponent } from '../acerca-de/acerca-de/acerca-de.component';
+
+
+
+@NgModule({
+  declarations: [
+    AcercaDeComponent
+  ],
+  imports: [
+    CommonModule,
+    AcercaDeRoutingModule
+  ]
+})
+export class AcercaDeModule { }
